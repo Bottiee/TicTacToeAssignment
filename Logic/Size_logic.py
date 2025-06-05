34380f2e44051049cb3045605131dce033eedf6b
Bottiee <siaulys.manfredas@gmail.com>
2025-06-05 12:24:27 +0300

    Code no longer needed for GUI functionality

--- Logic/Size_logic.py
+++ Logic/Size_logic.py
@@ -1,15 +1,15 @@
 # Size_logic.py
 
-class SizeLogic:
-    def __init__(self, initial_size=3):
-        self.available_sizes = [3, 4, 5]
-        self.selected_size = initial_size
-
-    def set_size(self, size):
-        if size in self.available_sizes:
-            self.selected_size = size
-        else:
-            raise ValueError(f"Unsupported grid size: {size}")
-
-    def get_size(self):
-        return self.selected_size
+# class SizeLogic:
+#     def __init__(self, initial_size=3):
+#         self.available_sizes = [3, 4, 5]
+#         self.selected_size = initial_size
+#
+#     def set_size(self, size):
+#         if size in self.available_sizes:
+#             self.selected_size = size
+#         else:
+#             raise ValueError(f"Unsupported grid size: {size}")
+#
+#     def get_size(self):
+#         return self.selected_size
